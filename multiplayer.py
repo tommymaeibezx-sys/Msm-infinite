deebc51ea59a8c640f3e5c79c1c10a07f435941a
tommymaeibezx-sys <tommymaeibezx@gmail.com>
2026-06-18 12:52:13 -0400

    Update multiplayer.py

--- multiplayer.py
+++ multiplayer.py
@@ -1,13 +1,10 @@
 players_online = {}
 
 def player_join(user_id):
-    players_online[user_id] = {
-        "user_id": user_id
-    }
+    players_online[user_id] = {"user_id": user_id}
 
 def player_leave(user_id):
-    if user_id in players_online:
-        del players_online[user_id]
+    players_online.pop(user_id, None)
 
 def get_online_players():
-    return list(players_online.values()) 
+    return list(players_online.values())
