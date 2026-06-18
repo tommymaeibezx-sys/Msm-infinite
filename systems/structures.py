7c8e41d37ed30e8e2c1a2495f60790578c5df778
tommymaeibezx-sys <tommymaeibezx@gmail.com>
2026-06-18 12:39:04 -0400

    Update structures.py

--- systems/structures.py
+++ systems/structures.py
@@ -4,10 +4,23 @@ def buy(player, req):
     structure = {
         "id": len(player["structures"]) + 1,
         "type": req.get("type", "nursery"),
-        "level": 1
+        "x": 0,
+        "y": 0
     }
 
     player["structures"].append(structure)
     save_player(player)
 
     return {"_cmd": "gs_update_structure", "structure": structure}
+
+
+def move(player, req):
+    sid = req.get("id")
+
+    for s in player["structures"]:
+        if s["id"] == sid:
+            s["x"] = req.get("x")
+            s["y"] = req.get("y")
+
+    save_player(player)
+    return {"_cmd": "gs_move_structure"}
